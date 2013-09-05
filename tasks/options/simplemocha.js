7ef481cb340edc37f6bb6c99024016e20092a21d
Thomas Boyt <me@thomasboyt.com>
2013-09-05 12:37:00 -0400

    remove debug AMD test

--- tasks/options/simplemocha.js
+++ tasks/options/simplemocha.js
@@ -10,10 +10,5 @@ module.exports = {
     reporter: 'tap'
   },
 
-  all: { src: ['test/.generated/**/*.js'] },
-  amd: { src: ['test/.generated/**/*.js'], 
-    options: {
-      grep: "AMD"
-    }
-  },
+  all: { src: ['test/.generated/**/*.js'] }
 };
