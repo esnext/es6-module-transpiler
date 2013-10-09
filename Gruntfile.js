9cef9c5e321cd9793d58d6b3ab8b0aa39a2d8e69
Thomas Boyt <me@thomasboyt.com>
2013-10-08 20:08:13 -0400

    Revert "Revert to old build task"
    This reverts commit cd6f88826c6ecdd7bde8c0cbfec6bd3ba867ea39.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,11 +24,8 @@ module.exports = function(grunt) {
   // Load local tasks.
   grunt.task.loadTasks('./tasks');
 
-  // This is really only here for illustrative purposes and can't be used as-is
-  // because of https://github.com/thlorenz/es6ify/issues/3.
-  //
-  // grunt.registerTask('build',
-  //   ['clean', 'transpile', 'es6ify', 'browserify', 'concat', 'uglify']);
+   grunt.registerTask('build',
+      ['clean', 'transpile', 'es6ify', 'browserify', 'concat', 'uglify']);
 
   grunt.registerTask('test', ['features', 'simplemocha']);
 
