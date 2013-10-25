ebabc14a6be569dc3f2c2512d4a18d90c64ae93b
Brian Donovan <donovan@squareup.com>
2013-10-25 18:24:17 -0400

    Print a warning when importing a module exported as a function.

--- test/features/new_default.cjs.js
+++ test/features/new_default.cjs.js
@@ -2,6 +2,14 @@
 var foo = (function() {
   var moduleInstanceObject = Object.create ? Object.create(null) : {};
   var imported = require("foo");
+  if (typeof imported === 'function' && typeof console !== 'undefined') {
+    var warning = "imported module 'foo' exported a function - this may not work as expected";
+    if (typeof console.warn === 'function') {
+      console.warn(warning);
+    } else if (typeof console.log === 'function') {
+      console.log(warning);
+    }
+  }
   for (var key in imported) {
     if (Object.prototype.hasOwnProperty.call(imported, key)) {
       moduleInstanceObject[key] = imported[key];
