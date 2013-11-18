9a1b51019ffe47258803cadc3f2abc8263aec3bb
Caridy Patino <caridy@gmail.com>
2013-11-18 15:55:36 -0500

    adding support for import { default as Ember } for yui modules, treating [default] as the especial case

--- test/features/import_default_as.yui.js
+++ test/features/import_default_as.yui.js
@@ -1,5 +1,5 @@
 YUI.add("import_default_as", function(Y, NAME, __imports__, __exports__) {
     "use strict";
-    var Ember = __imports__["ember"].default;
+    var Ember = __imports__["ember"]["default"];
     return __exports__;
 }, "@VERSION@", {"es":true,"requires":["ember"]});
