645eb1082ca3765906ed694d5079a95cd37df07b
Matthew Robb <matthewwrobb@gmail.com>
2014-08-13 17:27:39 -0400

    When verifying imports not reassigned, make sure the identifier isthe same as an import and make sure declaration scopes are global

--- test/examples/reassign-import-fails/importer.js
+++ test/examples/reassign-import-fails/importer.js
@@ -2,5 +2,10 @@
 
 import { x } from './exporter';
 
-/* error: type=SyntaxError message="Cannot reassign imported binding `x` at importer.js:6:1" */
+function $() {
+    for(var x = 0; x < 1; x++){}
+    for(var x = 0; x < 1; x++){}
+}
+
+/* error: type=SyntaxError message="Cannot reassign imported binding `x` at importer.js:11:1" */
 x = 10;
