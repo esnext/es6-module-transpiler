14777c0edf3ab1cc340d417bbea330aa27844ec3
Matthew Robb <matthewwrobb@gmail.com>
2014-08-14 00:52:40 -0400

    Added an additional test case for checking non top level scopes reassigning to imported binding identifiers

--- test/examples/reassign-import-fails/importer.js
+++ test/examples/reassign-import-fails/importer.js
@@ -2,10 +2,10 @@
 
 import { x } from './exporter';
 
-function $() {
+(function() {
     for(var x = 0; x < 1; x++){}
     for(var x = 0; x < 1; x++){}
-}
+});
 
 /* error: type=SyntaxError message="Cannot reassign imported binding `x` at importer.js:11:1" */
 x = 10;
