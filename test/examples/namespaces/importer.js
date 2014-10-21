4a59306943dca785213f9dc88ba4302fefc39060
Brian Donovan <donovan@squareup.com>
2014-10-20 19:10:44 -0700

    Check enumerability of the module exports.

--- test/examples/namespaces/importer.js
+++ test/examples/namespaces/importer.js
@@ -5,3 +5,9 @@ import * as foo from './exporter';
 assert.equal(foo['default'], 'DEF');
 assert.equal(foo.b, 'b');
 assert.equal(foo.a, 'a');
+
+var keys = [];
+for (var key in foo) {
+  keys.push(key);
+}
+assert.deepEqual(keys.sort(), ['a', 'b', 'default']);
